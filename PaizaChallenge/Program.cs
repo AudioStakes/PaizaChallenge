4c89fdc503280d2fb2ff4e80675cd60e3a265ad1
AudioStakes <audiostakes@gmail.com>
2019-03-15 11:40:58 +0900

    firstCommit

---
+++ PaizaChallenge/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PaizaChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            ChangeLocalDebugInput();
+
+            // your code
+            // ...
+        }
+
+        [System.Diagnostics.Conditional("LOCAL_DEBUG")]
+        static void ChangeLocalDebugInput()
+        {
+            Console.SetIn(new System.IO.StreamReader("sampleInput.txt"));
+        }
+    }
+}
